c824e5a154599c4fe10f46fafaee250a8592ae4d
zokir.js <zokirmahamadjonov29@gmail.com>
2022-03-14 05:19:13 +0500

    complited 60%

---
+++ base.js
@@ -0,0 +1,121 @@
+const data = [
+    {
+        code: 4,
+        description: "Dvigatel harorati yuqoriligi sabab konditsioner o'chirildi.",
+    },
+    {
+        code: 15,
+        description: "Yuqoridagi tormoz chirog'ida nosozlik.",
+    },
+    {
+        code: 16,
+        description: "Stop chiroqlarga texnik xizmat ko'rsating.",
+    },
+    {
+        code: 18,
+        description: "Chap taraf yaqinni yoritish chirog'ida nosozlik.",
+    },
+    {
+        code: 20,
+        description: "O'ng dara yaqinni yoritish chirog'ida nosozlik.",
+    },
+    {
+        code: 21,
+        description: "Chap old gabarit chiroqda nosozlik.",
+    },
+    {
+        code: 22,
+        description: "O'ng old gabarit chiroqda nosozlik.",
+    },
+    {
+        code: 23,
+        description: "Orqaga harakatlanish chirog'ida nosozlik.",
+    },
+    {
+        code: 24,
+        description: "Raqam belgisini yoritish chirog'ida nosozlik.",
+    },
+    {
+        code: 25,
+        description: "Chap old burilishni ko'rsatuvchi chiroqda nosozlik."
+    },
+    
+    {
+        code: 26,
+        description: "Chap orqa burilishni ko'rsatuvchi chiroqda nosozlik."
+    },
+    
+    {
+        code: 27,
+        description: "O'ng old burilishni ko'rsatuvchi chiroqda nosozlik."
+    },
+    
+    {
+        code: 28,
+        description: "O'ng orqa burilishni ko'rsatuvchi chiroqda nosozlik."
+    },
+    {
+        code: 35,
+        description: "Masofadan boshqaruv puliti batareyasini almashtiring."
+    },
+    {
+        code: 59,
+        description: "Haydovchi eshigi oynasini oching, so'ng yoping."
+    },
+    {
+        code: 60,
+        description: "Yo'lovchi eshigi oynasini oching, so'ng yoping."
+    },
+    {
+        code: 61,
+        description: "Chap orqa eshigi oynasini oching, so'ng yoping."
+    },
+    {
+        code: 62,
+        description: "O'ng orqa eshigi oynasini oching, so'ng yoping."
+    },
+    {
+        code: 65,
+        description: "O'g'rilik harakati sodir bo'ldi."
+    },
+    {
+        code: 66,
+        description: "O'grilikka qarshi signalizatsiyada nosozlik."
+    },
+    {
+        code: 75,
+        description: "Konditsioner tizimini tekshiring."
+    },
+    {
+        code: 79,
+        description: "Dvigitel moyini tekshiring."
+    },
+    {
+        code: 81,
+        description: "Avtomatik uzatmalar qutisini tekshiring."
+    },
+    {
+        code: 84,
+        description: "Dvigitelning quvvati pasaydi."
+    },
+    {
+        code: 89,
+        description: "O'grilikka qarshi signalizatsiyada nosozlik."
+    },
+    {
+        code: 94,
+        description: "Konditsioner tizimini tekshiring."
+    },
+    {
+        code: 95,
+        description: "Dvigitel moyini tekshiring."
+    },
+    {
+        code: 128,
+        description: "Avtomatik uzatmalar qutisini tekshiring."
+    },
+    {
+        code: 174,
+        description: "Dvigitelning quvvati pasaydi."
+    }
+]
